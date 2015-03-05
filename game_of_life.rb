2fa75a833bbec16e3d05af101efe1e0059bb1785
Joey <joey@Joeys-MacBook-Pro.local>
2015-03-04 22:15:21 -0700

    Mostly working

--- game_of_life.rb
+++ game_of_life.rb
@@ -24,40 +24,36 @@
 # Extra 02. Fix menus and presentation
 # Extra 03. Let user decide what percentage of board to fill with '0' at the beginning.
 # Extra 04. Move classes to files of their own
-require 'matrix'
+# require 'matrix'
 
-# @matrix = [
-#   [0,0,0,0,0,0,0,0,0,0],
-#   [0,0,0,0,0,0,0,0,0,0],
-#   [0,0,0,0,0,0,0,0,0,0],
-#   [0,0,0,0,0,0,0,0,0,0],
-#   [0,0,0,0,0,0,0,0,0,0],
-#   [0,0,0,0,0,0,0,0,0,0],
-#   [0,0,0,0,0,0,0,0,0,0],
-#   [0,0,0,0,0,0,0,0,0,0],
-#   [0,0,0,0,0,0,0,0,0,0],
-#   [0,0,0,0,0,0,0,0,0,0],
-# ]
+@alive = "⦿"
+@dead = " "
+@matrix = Array.new(50) {Array.new(50) {[@alive,@dead].sample}}
+# @matrix2 = Array.new(50) {Array.new(50) {" "}}
+@matrix2 = @matrix.dup
+@matrix3 = Array.new(50) {Array.new(50) {" "}}
 
-def display_matrix
-  @matrix.to_a.each do |row|
+def display_matrix(matrix)
+  matrix.each do |row|
     puts row.map { |col| col}.join(' ')
   end 
 end
 
-def rand_matrix
-  @matrix = Matrix.build(10) {rand(0..1)}
-end
-
-puts "The Game of Life"
-puts ""
-puts "Would you like to generate a new random matrix?"
-response = gets.chomp.downcase.to_s
-if response == "y"
-  rand_matrix
-  display_matrix
-else
-  puts "Too bad, it's really cool."
+# def rand_matrix
+  # @matrix = Matrix.build(10) {[@alive,@dead].sample}
+  # @matrix = @matrix.to_a
+# end
+def introduction
+  puts "The Game of Life"
+  puts ""
+  puts "Would you like to generate a new random matrix?"
+  response = gets.chomp.downcase.to_s
+  if response == "y"
+    looper
+    # display_matrix
+  else
+    puts "Too bad, it's really cool."
+  end
 end
 
 # a = display_matrix
@@ -77,36 +73,94 @@ end
 
 # puts ""
 
-# display_matrix.each_with_index do |row, row_index|
-#   row.each_with_index do |col, col_index|
-#   end
-# end
 
-def find_neighbors(x,y)
-  @neighbors = [
-    display_matrix[x -1][y-1],
-    display_matrix[x -1][y],
-    display_matrix[x -1][y+1],
-    display_matrix[x][y-1],
-    display_matrix[x][y+1],
-    display_matrix[x +1][y-1],
-    display_matrix[x +1][y],
-    display_matrix[x +1][y+1],
-  ]
-  return @neighbors
+def find_neighbors(row_index, col_index)
+  # neighbors = [
+  #   @matrix[row_index - 1 >= 0 ? row_index - 1 : 9] [col_index - 1 >= 0 ? col_index - 1 : 9],
+  #   @matrix[row_index - 1 >= 0 ? row_index - 1 : 9] [col_index],
+  #   @matrix[row_index - 1 >= 0 ? row_index - 1 : 9] [col_index + 1 < 10 ? col_index + 1 : 0],
+  #   @matrix[row_index]                              [col_index - 1 >= 0 ? col_index - 1 : 9],
+  #   @matrix[row_index]                              [col_index + 1 < 10 ? col_index + 1 : 0],
+  #   @matrix[row_index + 1 < 10 ? row_index + 1 : 0 ][col_index - 1 >= 0 ? col_index - 1 : 9],
+  #   @matrix[row_index + 1 < 10 ? row_index + 1 : 0 ][col_index],
+  #   @matrix[row_index + 1 < 10 ? row_index + 1 : 0 ][col_index + 1 < 10 ? col_index + 1 : 0]
+  # ]
+  # return neighbors
+    neighbors = [
+      @matrix[row_index - 1] [col_index - 1], 
+      @matrix[row_index]                             [col_index - 1], 
+      @matrix[row_index + 1 < 50 ? row_index + 1 : 0][col_index - 1], 
+      @matrix[row_index - 1] [col_index], 
+      @matrix[row_index + 1 < 50 ? row_index + 1 : 0][col_index], 
+      @matrix[row_index - 1] [col_index + 1 < 50 ? col_index + 1 : 0], 
+      @matrix[row_index]                             [col_index + 1 < 50 ? col_index + 1 : 0], 
+      @matrix[row_index + 1 < 50 ? row_index + 1 : 0][col_index + 1 < 50 ? col_index + 1 : 0]
+    ]
+
+  count_neighbors(neighbors)
 end
-puts ""
 
+# introduction
+#p find_neighbors(1,1)
 
+def count_neighbors(neighbors)
+  count = 0
+  neighbors.each do |cell|
+    count += 1 if cell == @alive
 
-# if display_matrix[x,y] == '1'
-#   neighobors.each do |cell|
-#   if cell == '1'
-#     count += 1
-#   else
-#     false
-#   end
+  end
+  return count
+end
+
+# p count_neighbors(find_neighbors(0, 0))
+
+# def count_array
+#   rand_matrix
+#   display_matrix
 # end
+
+def rules(x, y, alive_count)
+
+  if @matrix[x][y] == @alive
+    if alive_count < 2
+       @matrix2[x][y] = @dead
+    elsif alive_count == 2 || alive_count == 3
+      @matrix2[x][y] = @alive
+    elsif alive_count > 3
+       @matrix2[x][y] = @dead 
+    end
+  else 
+    if alive_count == 3
+      @matrix2[x][y] = @alive
+    end
+  end
+end
+
+def iterator
+  @matrix.each_with_index do |row, row_index|
+    row.each_with_index do |col, col_index|
+      alive_neighbor_count = find_neighbors(row_index, col_index)
+      @matrix3[row_index][col_index] = alive_neighbor_count
+      rules(row_index, col_index, alive_neighbor_count)
+    end
+  end
+end
+
+def looper()
+  # 2.times do
+  while true
+    system "clear"
+
+    iterator
+    @matrix = @matrix2.dup
+    display_matrix(@matrix3)
+    display_matrix(@matrix)
+    sleep 0.3
+  end
+end
+
+introduction
+# looper()
 
 # def cells_that_should_die
 #   if x == 1 && count(0..1) then x = 0 ||
